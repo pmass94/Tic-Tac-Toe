2acb79e7412f543a1ddb8fa02963fa9d98725b0d
Peter Mass <mass.peter@web.de>
2021-07-14 14:24:18 +0200

    version update

--- script.js
+++ script.js
@@ -34,18 +34,58 @@ function draw() {
 
 function checkForWin() {
     let winner;
+    // first horizontal row
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+        winner = fields[0];
+        document.getElementById('line-0').style.transform = 'scaleX(1)';
+    }
 
-    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) { winner = fields[0]; }
-    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) { winner = fields[3]; }
-    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) { winner = fields[6]; }
-    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) { winner = fields[0]; }
-    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) { winner = fields[1]; }
-    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) { winner = fields[2]; }
-    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) { winner = fields[0]; }
-    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) { winner = fields[2]; }
+    // second horizontal row
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
+    }
 
+    //third horizontal row
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        winner = fields[6];
+        document.getElementById('line-2').style.transform = 'scaleX(1)';
+    }
+
+    // first vertical row
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) { 
+        winner = fields[0]; 
+        document.getElementById('line-3').style.transform = 'scaleX(1) rotate(90deg)';
+    }
+
+    // second vertical row
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) { 
+        winner = fields[1]; 
+        document.getElementById('line-4').style.transform = 'scaleX(1) rotate(90deg)';
+    }
+
+    // third vertical row
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) { 
+        winner = fields[2]; 
+        document.getElementById('line-5').style.transform = 'scaleX(1) rotate(90deg)';
+    }
+
+    // first diagonal row
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) { 
+        winner = fields[0]; 
+        document.getElementById('line-6').style.transform = 'scaleX(1) rotate(45deg)';
+    }
+
+    //second diagonal row
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) { 
+        winner = fields[2]; 
+        document.getElementById('line-7').style.transform = 'scaleX(1) rotate(-45deg)';
+    }
+
+
+    // console log
     if (winner) {
-        console.log('Master!', winner);
+        console.log('You are the champ!', winner);
     }
 
 }
