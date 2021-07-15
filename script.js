85479dd7e599d7dfef9cbf06ff4f7324bc273b52
Peter Mass <mass.peter@web.de>
2021-07-15 09:55:11 +0200

    version update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let fields = [];
+let gameOver = false;
+let audio_win = new Audio('audio/winner.mp3');
 
 let currentShape = 'cross';
 
 function fillShape(id) {
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) {
         if (currentShape == 'cross') {
             currentShape = 'circle';
             document.getElementById('player-1').classList.add('player-inactive');
@@ -53,39 +55,68 @@ function checkForWin() {
     }
 
     // first vertical row
-    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) { 
-        winner = fields[0]; 
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
         document.getElementById('line-3').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
     // second vertical row
-    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) { 
-        winner = fields[1]; 
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
         document.getElementById('line-4').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
     // third vertical row
-    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) { 
-        winner = fields[2]; 
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
         document.getElementById('line-5').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
     // first diagonal row
-    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) { 
-        winner = fields[0]; 
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
         document.getElementById('line-6').style.transform = 'scaleX(1) rotate(45deg)';
     }
 
     //second diagonal row
-    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) { 
-        winner = fields[2]; 
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+        winner = fields[2];
         document.getElementById('line-7').style.transform = 'scaleX(1) rotate(-45deg)';
     }
 
 
-    // console log
+    // winner of the game
     if (winner) {
         console.log('You are the champ!', winner);
+        gameOver = true;
+
+        setTimeout(function () {
+            document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-div').classList.remove('d-none');
+            audio_win.play();
+        }, 1000);
+
     }
+}
+
+function restart() {
+    gameOver = false;
+    fields = [];
+
+
+    for (let i = 0; i < 8; i++) {
+        document.getElementById('line-' + i).style.transform = 'scaleX(0)';
+    }
+
+    setTimeout(function () {
+        document.getElementById('game-over').classList.add('d-none');
+        document.getElementById('restart-div').classList.add('d-none');
+
+
+        for (let i = 0; i < 9; i++) {
+            document.getElementById('circle-' + i).classList.add('d-none');
+            document.getElementById('cross-' + i).classList.add('d-none');
+        }
+    }, 225);
 
 }
